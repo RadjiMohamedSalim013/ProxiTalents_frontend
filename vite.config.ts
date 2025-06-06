652ae9b35c27ddafc884a4338221e825934763d0
Radji Mohamed <radjimohamed013@gmail.com>
2025-06-06 22:22:40 +0100

    correction du bug lors de l'hbergement

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-
   ],
   server: {
-    // React Router: history fallback pour que toutes les routes servent index.html
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
     fs: {
       strict: false
     }
-  },
+  }
 })
